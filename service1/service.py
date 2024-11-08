968132afb2ff1c7af1598744fcc38e22a19a1ac7
Khanh Dang <khanh.dang@reaktor.com>
2024-11-08 22:42:50 +0200

    exercise4

--- service1/service.py
+++ service1/service.py
@@ -2,6 +2,9 @@ from flask import Flask, jsonify
 import os
 import requests
 import socket
+import time
+import sys
+import subprocess
 
 app = Flask(__name__)
 
@@ -31,11 +34,30 @@ def get_service2():
 def get_info():
     service1_data = get_container_info()
     service2_data = get_service2()
+    time.sleep(2) # Delay 2 seconds
     response = {
         "Service1": service1_data,
         "Service2": service2_data
     }
     return jsonify(response)
+
+@app.route('/stop', methods=['POST'])
+def stop():
+    print("Service stop requested, shutting down all containers.", file=sys.stderr)
+    
+    # Respond to client immediately before shutdown
+    response = jsonify({"message": "Service is stopping all containers."})
+    response.status_code = 200
+    
+    # Shut down the Docker Compose process after sending the response
+    # Use a subprocess to run the shutdown command
+    shutdown_cmd = "docker-compose down"
+    try:
+        subprocess.Popen(shutdown_cmd, shell=True)
+    except Exception as e:
+        print(f"Failed to shut down: {e}", file=sys.stderr)
+    
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
