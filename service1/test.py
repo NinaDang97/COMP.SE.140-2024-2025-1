6cadc1aac4b54c48f5d457d50270598addf05d7e
Khanh Dang <khanh.dang@reaktor.com>
2025-01-27 23:16:52 +0200

    Write tests for GET /state and PUT /state

--- service1/test.py
+++ service1/test.py
@@ -6,7 +6,7 @@ BASE_URL = 'http://localhost:8197' # URL for service1
 # Mock test
 def test():
     assert 1 == 1
-    
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
@@ -18,4 +18,19 @@ def test_get_info_data(client):
     data = response.get_json()
     assert "Service1" in data
     assert "Service2" in data
+
+def test_get_state(client):
+    response = client.get('/state')
+    assert response.status_code == 200
+    assert response.get_json() == "INIT"
+
+def test_update_state(client):
+    response = client.post('/state', data="RUNNING")
+    assert response.status_code == 200
+    assert response.get_json() == "RUNNING"
+
+def test_invalid_state(client):
+    response = client.post('/state', data="INVALID")
+    assert response.status_code == 400
+    assert response.get_json() == {"error": "Invalid state"}
 
